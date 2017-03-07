94f2ab8658766ef77cb85d3de429b688ca04d44b
ליחי אקוע <lihai2511@gmail.com>
2017-03-07 20:23:13 +0200

    ionic deploy active

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import {Deploy} from '@ionic/cloud-angular';
+
 
 //import { TabsPage } from '../pages/tabs/tabs';
 import { MenuPage } from '../menu/menu.component.ts';
@@ -14,12 +16,31 @@ import {LotteryApi} from '../services/lottery.service.ts'
 export class MyApp {
   rootPage = MenuPage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, deploy: Deploy) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+
+      if (!platform.is('core')) {
+        // This will only print when running on desktop
+        console.log("I'm a regular browser!");
+        deploy.channel = 'dev';
+        deploy.check().then((snapshotAvailable: boolean) => {
+          alert('checked! '+snapshotAvailable);
+          if (snapshotAvailable) {
+            // When snapshotAvailable is true, you can apply the snapshot
+            deploy.download().then(() => {
+              return deploy.extract();
+            }).then(()=>{deploy.load();});
+          }
+        });
+      }
+
+
     });
+
+
   }
 }
